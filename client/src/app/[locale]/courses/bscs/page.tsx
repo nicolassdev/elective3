5144eba612b30f5593ed45588ba93e80bdb800bb
Toy-123 <Lukemusic001@gmail.com>
2025-01-25 16:20:48 +0800

    ADDED SSSSSSSSS

--- client/src/app/[locale]/courses/bscs/page.tsx
+++ client/src/app/[locale]/courses/bscs/page.tsx
@@ -2,7 +2,8 @@ export default function contact(){
   return (
     <div>
       <h1>Compute Systems Institute </h1>
-      <b>Bachelor of Science in Computer Science</b>
+      <b>Bachelor of Science in Computer Science BSCS BSCS SSSSSSSSSSSSSSS</b>
+
     </div>
 
   )
