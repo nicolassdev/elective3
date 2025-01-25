7834b47f16a2e6f3bb074ee3edc7eb933fcf49a9
Anthony <anthonydaen25@gmail.com>
2025-01-25 16:09:03 +0800

    Added courses

---
+++ client/src/app/[locale]/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function contact(){
+  return (
+    <div>
+      <h1>THIS IS THE CONTACT NUMBER </h1>
+    </div>
+
+  )
+}
---
+++ client/src/app/[locale]/courses/bsis/page.tsx
@@ -0,0 +1,9 @@
+export default function contact(){
+  return (
+    <div>
+      <h1>Compute Systems Institute </h1>
+      <b>Bachelor of Science in Information System</b>
+    </div>
+
+  )
+}
