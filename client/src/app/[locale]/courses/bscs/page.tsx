d11c6816bfe100da31e99e7a0bac5381579cd560
Anthony <anthonydaen25@gmail.com>
2025-01-26 23:12:42 +0800

    Added courses and contact

--- client/src/app/[locale]/courses/bscs/page.tsx
+++ client/src/app/[locale]/courses/bscs/page.tsx
@@ -1,10 +1,8 @@
-export default function contact(){
+export default function contact() {
   return (
     <div>
       <h1>Compute Systems Institute </h1>
       <b>Bachelor of Science in Computer Science BSCS BSCS SSSSSSSSSSSSSSS</b>
-
     </div>
-
-  )
+  );
 }
