d673c0d4742f1ddcec0b67ceb808d230938f82b9
Anthony <anthonydaen25@gmail.com>
2025-02-08 19:24:33 +0800

    added login form

--- client/src/app/[locale]/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function contact(){
-  return (
-    <div>
-      <h1>THIS IS THE CONTACT NUMBER </h1>
-    </div>
-
-  )
-}
--- client/src/app/[locale]/courses/bscs/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function contact() {
-  return (
-    <div>
-      <h1>Compute Systems Institute </h1>
-      <b>Bachelor of Science in Computer Science BSCS BSCS SSSSSSSSSSSSSSS</b>
-    </div>
-  );
-}
